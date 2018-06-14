6b378b73cb0ec9ae55e8f50a060b73509ed7a6a3
Ari Fogel <ari@fogelti.me>
2018-06-13 21:40:56 -0700

    first attempt

---
+++ pmd-unused-private-method-test/src/main/java/org/arifogel/pmd_unused_private_method_test/MyClass.java
@@ -0,0 +1,6 @@
+package org.arifogel.pmd_unused_private_method_test;
+
+public class MyClass {
+  public static void main(int argc, String[] args) {
+  }
+}
---
+++ pmd-unused-private-method-test/src/test/java/org/arifogel/pmd_unused_private_method_test/MyClassTest.java
@@ -0,0 +1,9 @@
+package org.arifogel.pmd_unused_private_method_test;
+
+import org.junit.Test;
+
+public class MyClassTest {
+  @Test
+  public void testStuff() {
+  }
+}
