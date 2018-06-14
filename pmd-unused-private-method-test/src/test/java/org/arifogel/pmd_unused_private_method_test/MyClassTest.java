c717e0e32cd3562deb0044c6666fcc4f280e30a3
Ari Fogel <ari@fogelti.me>
2018-06-13 21:48:33 -0700

    install works

--- pmd-unused-private-method-test/src/test/java/org/arifogel/pmd_unused_private_method_test/MyClassTest.java
+++ pmd-unused-private-method-test/src/test/java/org/arifogel/pmd_unused_private_method_test/MyClassTest.java
@@ -1,9 +1,13 @@
 package org.arifogel.pmd_unused_private_method_test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.Test;
 
 public class MyClassTest {
   @Test
   public void testStuff() {
+    assertThat(true, equalTo(true));
   }
 }
